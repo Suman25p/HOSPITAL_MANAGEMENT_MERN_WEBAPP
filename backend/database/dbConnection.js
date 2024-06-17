f60a6ba65eb492193d9307c0725e3d35c365b644
Suman Pasi_2051235 <128914804+Suman25p@users.noreply.github.com>
2024-06-17 10:15:08 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "HOSPITAL_MANAGEMENT_MERN_DATABASE",
+      dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_DEPLOYED",
     })
     .then(() => {
       console.log("Connected to Database!");
